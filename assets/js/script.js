d163cd8d175efc8f5eca77755cf9b8f627f3f8b6
Enrique Jorquera <enrique.jorqueram@gmail.com>
2024-02-04 19:55:45 -0300

    Carga inicial del Proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var cadena = "hola mundo";
+var a =100;
+var b = 1.5;
+
+console.log("Cadena: " , cadena);
+console.log("Valor de a:" , a);
+console.log("Valor de a:" , b);
+
+cadena = "Hola Chile";
+a=200;
+b=2.5; 
+console.log("Cadena: " , cadena);
+console.log("valor de a: ", a);
+console.log("Valor de b: ", b);
+
+var c,d,e,f;
+
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+
+console.log("Valor de c: " , c);
+console.log("Valor de d: " , d);
